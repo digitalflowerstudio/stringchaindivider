5dd34d22318af4568d910b21f6d64796a1ead070
digitalflowerstudio <floriantappeser@gmail.com>
2023-12-14 13:35:00 +0100

    added return statements

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,10 +20,12 @@ const divideString = () => {
     resultBack = inputString.slice(inputIndex);
     outputFront.innerHTML = `<h2>First Part</h2><br><p>${resultFront}</p>`;
     outputBack.innerHTML = `<h2>Last Part</h2><br><p>${resultBack}</p>`;
+    return;
   } else if (after) {
     resultFront = inputString.slice(0, inputIndex + 1);
     resultBack = inputString.slice(inputIndex + 1);
     outputFront.innerHTML = `<h2>First Part</h2><br><p>${resultFront}</p>`;
     outputBack.innerHTML = `<h2>Last Part</h2><br><p>${resultBack}</p>`;
+    return;
   }
 };
